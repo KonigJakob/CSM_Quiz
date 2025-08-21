0cd2093db5f26271f530b35b529d71e327cd2440
Jaimemilio <105794884+KonigJakob@users.noreply.github.com>
2025-08-21 15:03:58 +0200

    add player prefs

--- csm_quiz/scripts/quiz_manager.gd
+++ csm_quiz/scripts/quiz_manager.gd
@@ -11,6 +11,8 @@ var question_sets = {
 var current_set : int = KEY_F1
 var current_language : String = "en"  # logical language key
 
+func _ready() -> void:
+	LoadPrefs()
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event is InputEventKey:
@@ -23,9 +25,32 @@ func switch_question_set(key : int) -> void:
 	TranslationServer.set_locale(new_locale)
 	set_changed.emit()
 	QuestionsLoader.reset_questions()
+	SavePrefs()
 
 func set_language(lang : String) -> void:
 	current_language = lang
 	var new_locale = question_sets[current_set][current_language]
 	TranslationServer.set_locale(new_locale)
 	QuestionsLoader.reset_questions()
+	SavePrefs()
+	
+func SavePrefs() -> void:
+	var configFile = ConfigFile.new()
+	configFile.set_value("Admin", "Set", current_set)
+	configFile.set_value("Admin", "Language", current_language)
+	configFile.save("user://configFile.cfg")
+	pass
+
+func LoadPrefs() -> void:
+	var configFile = ConfigFile.new()
+	# Load data from a file.
+	var err = configFile.load("user://configFile.cfg")
+	# If the file didn't load, ignore it.
+	if err != OK:
+		print("Prefs loaded unsuccesfully.")
+		return
+	current_language = configFile.get_value("Admin", "Language")
+	current_set = configFile.get_value("Admin", "Set")
+	switch_question_set(current_set)
+	set_language(current_language)
+	print("Prefs loaded succesfully.")
